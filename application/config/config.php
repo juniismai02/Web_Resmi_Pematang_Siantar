108d02dbba1680727cd189dd10d67bb309eadbc3
Juni Ismail <78461380+juniismai02@users.noreply.github.com>
2023-10-09 20:21:18 +0700

    Web Resmi V-1

--- application/config/config.php
+++ application/config/config.php
@@ -377,13 +377,13 @@ $config['encryption_key'] = 'TRUE';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'files';
-$config['sess_cookie_name'] = 'ci_session';
-$config['sess_expiration'] = 0;
-$config['sess_save_path'] = NULL;
-$config['sess_match_ip'] = FALSE;
-$config['sess_time_to_update'] = 300;
-$config['sess_regenerate_destroy'] = FALSE;
+$config['sess_driver']             = 'files';
+$config['sess_cookie_name']        = 'pcfx_session_';
+$config['sess_expiration']         = 7200;
+$config['sess_save_path']          = APPPATH.'writable';
+$config['sess_match_ip']           = FALSE;
+$config['sess_time_to_update']     = 300;
+$config['sess_regenerate_destroy'] = FALSE; 
 
 /*
 |--------------------------------------------------------------------------
