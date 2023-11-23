ae786858656b045014cde32d40beebbb0df16091
Juni Ismail <78461380+juniismai02@users.noreply.github.com>
2023-11-23 09:10:42 +0700

    Update DB

--- database/db_pematangsiantar.sql
+++ database/db_pematangsiantar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 23, 2023 at 02:56 AM
+-- Generation Time: Nov 23, 2023 at 03:06 AM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 7.4.21
 
